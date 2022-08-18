505e5e6c0a1ff14b4a621413954517d0e02be0c9
soham <sohampatel9403@gmail.com>
2022-08-18 13:28:48 +0530

    call back

--- index.js
+++ index.js
@@ -32,206 +32,213 @@ mongoose
   })
 
 export const port = process.env.PORT || '8000'
-try{
-app.use(express.static('public'))
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(fileUpload())
+try {
+  app.use(express.static('public'))
+  app.use(cors())
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(fileUpload())
 
-app.use('/api/', indexroute)
+  app.use('/api/', indexroute)
 
-httpServer.listen(port, () => {
-  console.log('APP is running on port ' + port)
-})
-
-
-
-const io = new Server(httpServer, { cors: '*' })
-
-io.on('connection', socket => {
-  console.log('user connected')
-  socket.on('user_online', async userdata => {
-    userdata = JSON.parse(userdata)
-    socket.user = { ...userdata }
+  httpServer.listen(port, () => {
+    console.log('APP is running on port ' + port)
   })
 
-  socket.on('create_room', async (data, callback) => {
-    try {
-      const { name, users, creator } = JSON.parse(data)
+  const io = new Server(httpServer, { cors: '*' })
 
-      if (name == '') {
-        callback({
-          status: 0,
-          message: 'Rooom name is required'
-        })
+  io.on('connection', socket => {
+    console.log('user connected')
+    socket.on('user_online', async userdata => {
+      userdata = JSON.parse(userdata)
+      socket.user = { ...userdata }
+    })
 
-        return
-      }
-      if (users.length < 0) {
-        callback({
-          status: 0,
-          message: 'Add users to room'
-        })
+    socket.on('create_room', async (data, callback) => {
+      try {
+        const { name, users, creator } = JSON.parse(data)
 
-        return
-      }
-      if (
-        !creator ||
-        (creator && (creator.usercode == '' || creator.id == ''))
-      ) {
-        callback({
-          status: 0,
-          message: 'No creator found'
-        })
+        if (name == '') {
+          callback({
+            status: 0,
+            message: 'Rooom name is required'
+          })
 
-        return
-      }
-      users.push(creator.usercode)
-
-      const fetchedusers = await UserModel.find(
-        { usercode: { $in: users } },
-        { name: 1, usercode: 1 }
-      )
-
-      const room = await RoomsModal.create({
-        name,
-        users: fetchedusers,
-        createdBy: mongoose.Types.ObjectId(creator.id)
-      })
-
-      await UserModel.updateMany(
-        { usercode: { $in: users } },
-        {
-          $push: {
-            rooms: { _id: mongoose.Types.ObjectId(room._id), name: room.name }
-          }
+          return
         }
-      )
+        if (users.length < 0) {
+          callback({
+            status: 0,
+            message: 'Add users to room'
+          })
 
-      io.sockets.sockets.forEach(users_socket_instance => {
+          return
+        }
         if (
-          users_socket_instance.user &&
-          users.includes(users_socket_instance.user.usercode)
+          !creator ||
+          (creator && (creator.usercode == '' || creator.id == ''))
         ) {
-          
-          users_socket_instance.join(room._id.toString())
+          callback({
+            status: 0,
+            message: 'No creator found'
+          })
+
+          return
         }
-      })
-      
-      socket.broadcast.to(room._id.toString()).emit('room_created', room)
-      callback({
-        status: 1,
-        message: 'Room created'
-      })
-    } catch (e) {
-      callback({
-        status: 0,
-        message: e.message
-      })
-    }
-  })
+        users.push(creator.usercode)
 
-  socket.on('join_room', async data => {
-    let { room } = data
-    socket.join(room)
-    socket.broadcast.to(room).emit(socket.user.name + ' joinded the chat')
-  })
+        const fetchedusers = await UserModel.find(
+          { usercode: { $in: users } },
+          { name: 1, usercode: 1 }
+        )
 
-  socket.on('join_rooms', async (data, callback) => {
-    try {
-      let { rooms } = data
+        const room = await RoomsModal.create({
+          name,
+          users: fetchedusers,
+          createdBy: mongoose.Types.ObjectId(creator.id)
+        })
 
-      for (let room of rooms) {
-        socket.join(room)
-        socket.broadcast.to(room).emit('user_chat_joined', {
-          roomId: room,
-          message: 'new user joined chat'
+        await UserModel.updateMany(
+          { usercode: { $in: users } },
+          {
+            $push: {
+              rooms: { _id: mongoose.Types.ObjectId(room._id), name: room.name }
+            }
+          }
+        )
+
+        io.sockets.sockets.forEach(users_socket_instance => {
+          if (
+            users_socket_instance.user &&
+            users.includes(users_socket_instance.user.usercode)
+          ) {
+            users_socket_instance.join(room._id.toString())
+          }
+        })
+
+        socket.broadcast.to(room._id.toString()).emit('room_created', room)
+        callback({
+          status: 1,
+          message: 'Room created'
+        })
+      } catch (e) {
+        callback({
+          status: 0,
+          message: e.message
         })
       }
+    })
 
-      callback({
-        status: 1,
-        message: 'all rooms joined'
+    socket.on('join_room', async data => {
+      let { room } = data
+      socket.join(room)
+      socket.broadcast.to(room).emit(socket.user.name + ' joinded the chat')
+    })
+
+    socket.on('join_rooms', async (data, callback) => {
+      try {
+        let { rooms } = data
+
+        for (let room of rooms) {
+          socket.join(room)
+          socket.broadcast.to(room).emit('user_chat_joined', {
+            roomId: room,
+            message: 'new user joined chat'
+          })
+        }
+        if (callback && typeof callback === 'function') {
+          callback({
+            status: 1,
+            message: 'all rooms joined'
+          })
+        }
+
+        return
+      } catch (e) {
+        if (callback && typeof callback === 'function') {
+          callback({
+            status: 0,
+            message: e.message
+          })
+        }
+        return
+      }
+    })
+
+    socket.on('message', async data => {
+      let { room, message } = data
+      socket.broadcast.to(room).emit('chat', {
+        roomId: room,
+        message,
+        user: socket.user.name
       })
+    })
 
-      return
-    } catch (e) {
-      callback({
-        status: 0,
-        message: e.message
+    socket.on('pressbutton', async (data, callback) => {
+      try {
+        let { room, message } = data
+        socket.broadcast.to(room).emit('pressbutton', {
+          roomId: room,
+          message
+        })
+        if (callback && typeof callback === 'function') {
+        callback({
+          status: 1,
+          message: ''
+        })
+      }
+      } catch (e) {
+        if (callback && typeof callback === 'function') {
+        callback({
+          status: 0,
+          message: e.message
+        })
+      }
+      }
+    })
+    socket.on('releasebutton', async (data, callback) => {
+      try {
+        let { room, message } = data
+        socket.broadcast.to(room).emit('releasebutton', {
+          roomId: room,
+          message
+        })
+         if (callback && typeof callback === 'function') {
+        callback({
+          status: 1,
+          message: ''
+        })
+      }
+      } catch (e) {
+         if (callback && typeof callback === 'function') {
+        callback({
+          status: 0,
+          message: e.message
+        })
+      }
+      }
+    })
+
+    socket.on('audio', async data => {
+      let { room, audio } = data
+
+      socket.broadcast.to(room).emit('audio', {
+        roomId: room,
+        audio
       })
-      return
-    }
-  })
+    })
 
-  socket.on('message', async data => {
-    let { room, message } = data
-    socket.broadcast.to(room).emit('chat', {
-      roomId: room,
-      message,
-      user: socket.user.name
+    socket.on('disconnect', () => {
+      const user = socket.user
     })
   })
 
-  socket.on('pressbutton', async (data, callback) => {
-    try {
-      let { room, message } = data
-      socket.broadcast.to(room).emit('pressbutton', {
-        roomId: room,
-        message
-      })
-      callback({
-        status: 1,
-        message: ''
-      })
-    } catch (e) {
-      callback({
-        status: 0,
-        message: e.message
-      })
-    }
+  io.engine.on('connection_error', err => {
+    console.log(err.req) // the request object
+    console.log(err.code) // the error code, for example 1
+    console.log(err.message) // the error message, for example "Session ID unknown"
+    console.log(err.context) // some additional error context
   })
-  socket.on('releasebutton', async (data, callback) => {
-    try {
-      let { room, message } = data
-      socket.broadcast.to(room).emit('releasebutton', {
-        roomId: room,
-        message
-      })
-      callback({
-        status: 1,
-        message: ''
-      })
-    } catch (e) {
-      callback({
-        status: 0,
-        message: e.message
-      })
-    }
-  })
-
-  socket.on('audio', async data => {
-    let { room, audio } = data
-
-    
-    socket.broadcast.to(room).emit('audio', {
-      roomId: room,
-      audio
-    })
-  })
-
-  socket.on('disconnect', () => {
-    const user = socket.user
-  })
-})
-
-io.engine.on('connection_error', err => {
-  console.log(err.req) // the request object
-  console.log(err.code) // the error code, for example 1
-  console.log(err.message) // the error message, for example "Session ID unknown"
-  console.log(err.context) // some additional error context
-})
-}catch(e){
-   app.response.send(e.message)
+} catch (e) {
+  app.response.send(e.message)
 }
